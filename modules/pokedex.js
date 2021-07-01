87a8d6a690217622473baa1753492fa8f1082d34
edisonknewgitlab <edisonknew@gmail.com>
2021-07-01 16:51:50 +0200

    Ajouts listes attaques

--- modules/pokedex.js
+++ modules/pokedex.js
@@ -1,17 +1,17 @@
-import {attaques} from './attaqueListe.js';
+import {attaques} from './attaques.js';
 import {Pokemon} from './pokemon.js';
 
 export const Evoli = new Pokemon("Evoli", 133, 6.5, "normal", 350);
-Evoli.attaque1 = attaques.find(attaque =>attaque.libelle ==="anticipation");
-Evoli.attaque2 = attaques.find(attaque =>attaque.libelle ==="adaptabilité");
+Evoli.attaque1 = attaques.anticipation;
+Evoli.attaque2 = attaques.adaptabilite;
 
 export const Pikachu = new Pokemon("Pikachu", 25, 6, "électrique", 82);
-Pikachu.attaque1 = attaques.find(attaque =>attaque.libelle ==="statik");
-Pikachu.attaque2 =attaques.find(attaque =>attaque.libelle ==="paratonnerre");
+Pikachu.attaque1 = attaques.statik;
+Pikachu.attaque2 =attaques.paratonnerre;
 
 export const Mewtwo = new Pokemon("Mewtwo", 25, 6, "mental", 82);
-Mewtwo.attaque1 = attaques.find(attaque =>attaque.libelle ==="Choc mental");
-Mewtwo.attaque2 =attaques.find(attaque =>attaque.libelle ==="Insta Win");
+Mewtwo.attaque1 = attaques.chocMental;
+Mewtwo.attaque2 =attaques.instaWin;
 
 export const pokemons =[Evoli,Pikachu,Mewtwo];
 
